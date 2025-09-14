cd403730e613f74ada8842a36aa87f8f9d164cfa
MCMHIPOLITO <132512852+MCMHIPOLITO@users.noreply.github.com>
2025-09-14 19:59:00 -0300

    Delete MCM-V1854_1409_1903-main/MCM-V1854_1409_1828-main/MCM-V1854_1209_1317-main/MCM-V1109_1854-main/sportmonks-live-ui-lastupdated-fix directory

--- MCM-V1854_1409_1903-main/MCM-V1854_1409_1828-main/MCM-V1854_1209_1317-main/MCM-V1109_1854-main/sportmonks-live-ui-lastupdated-fix/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import ErrorBoundary from '../components/ErrorBoundary';
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <ErrorBoundary><Component {...pageProps} /></ErrorBoundary>;
-}
